8121427d1ce41e8902ae58a71f44694eae3b8c3b
wangwenyan010928 <wangwenyan173@gmail.com>
2024-09-08 17:21:47 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function showContent(section) {
-    // 隐藏所有的内容块
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('acidRain').style.display = 'none';
-    document.getElementById('highAltitude').style.display = 'none';
-    document.getElementById('monitoring').style.display = 'none';
-    document.getElementById('contactUs').style.display = 'none';
+    // 获取所有的内容块
+    var sections = document.querySelectorAll(".content div");
+    
+    // 隐藏所有内容块
+    sections.forEach(function(div) {
+        div.style.display = 'none';
+    });
 
-    // 显示被点击的内容块
+    // 显示所点击的内容块
     document.getElementById(section).style.display = 'block';
 }
