118885c4278d3367b7d6cfc0abe738ed1ed9d8a1
wangwenyan010928 <wangwenyan173@gmail.com>
2024-09-08 19:24:53 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 function showContent(section) {
-    // 获取所有的内容块
+    // 隐藏所有的内容块
     var sections = document.querySelectorAll(".content div");
-    
-    // 隐藏所有内容块
     sections.forEach(function(div) {
         div.style.display = 'none';
     });
