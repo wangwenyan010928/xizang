4e2719988ceaaf2d9983caffb1e5cc5a10683f77
wangwenyan010928 <wangwenyan173@gmail.com>
2024-09-09 08:47:58 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,4 +7,25 @@ function showContent(section) {
 
     // 显示所点击的内容块
     document.getElementById(section).style.display = 'block';
+
+    // 首页时，隐藏侧边栏
+    var sidebar = document.getElementById('sidebar');
+    if (section === 'home') {
+        sidebar.style.display = 'none'; // 首页不显示侧边栏
+    } else if (section === 'acidRain') {
+        // 显示酸雨专题的侧边栏
+        sidebar.style.display = 'block';
+        sidebar.innerHTML = `
+            <ul>
+                <li><a href="#">业务规范</a></li>
+                <li><a href="#">规章制度</a></li>
+                <li><a href="#">业务工作</a></li>
+                <li><a href="#">计量检定</a></li>
+                <li><a href="#">质量体系内审检查</a></li>
+            </ul>
+        `;
+    } else {
+        // 显示其他内容时隐藏侧边栏
+        sidebar.style.display = 'none';
+    }
 }
